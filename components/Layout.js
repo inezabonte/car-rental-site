067f3ca79dd702341e4e2bc5ec50b858eec8c79a
inezabonte <inezabonte@gmail.com>
2021-09-08 16:07:13 +0200

    changed file name

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./navbar";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
 	return (
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Navbar() {
 	return (
 		<div className="flex justify-center">
-			<Image src={logo} width={200} height={200} />
+			<Image src={logo} width={80} height={80} className="text-white" />
 		</div>
 	);
 }
