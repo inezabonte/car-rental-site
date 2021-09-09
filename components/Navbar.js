6ef54f9b45cec4362e1b572df51dcf6bcfb87f57
inezabonte <inezabonte@gmail.com>
2021-09-09 19:43:52 +0200

    created slug page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,15 @@
 import logo from "public/images/logo.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
 	return (
 		<div className="flex justify-center">
-			<Image src={logo} width={80} height={80} className="text-white" />
+			<Link href="/">
+				<a>
+					<Image src={logo} width={80} height={80} className="text-white" />
+				</a>
+			</Link>
 		</div>
 	);
 }
